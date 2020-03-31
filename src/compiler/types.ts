b8da8955f892631305bc55561c7b8230ca8f5a44
Thibault Malbranche <thibault.malbranche@epitech.eu>
2020-03-31 17:19:37 +0200

    Add platform logic

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -5728,6 +5728,7 @@ namespace ts {
         incremental?: boolean;
         tsBuildInfoFile?: string;
         removeComments?: boolean;
+        resolutionPlatforms?: string[]; // Use react-native lookup logic for these platforms
         rootDir?: string;
         rootDirs?: string[];
         skipLibCheck?: boolean;
